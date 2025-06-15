a2fdca7fb960c8e7ac5f764a456e3cdcb852cd97
Adrian Oliveira <adrianoliv.contato@gmail.com>
2025-06-15 15:20:34 -0300

    backend2.0

--- backend/api.js
+++ backend/api.js
@@ -40,7 +40,7 @@ app.post('/veiculo', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor backend rodando na porta ${PORT}`);
 });
